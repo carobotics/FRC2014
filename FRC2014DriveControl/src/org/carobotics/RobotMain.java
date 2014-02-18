a3ff84b08ce5c86fce1cd08443da7da4c150590b
Stuart Hunt <onno.owl@gmail.com>
2014-02-17 21:18:38 -0500

    Added full control functionality. Arms are controllable, compressor can be turned on and off, catapult can be launched, right throttle can control speed, and left throttle toggles which side is the front.

--- FRC2014DriveControl/src/org/carobotics/RobotMain.java
+++ FRC2014DriveControl/src/org/carobotics/RobotMain.java
@@ -8,14 +8,17 @@
 package org.carobotics;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.camera.AxisCamera;
 import edu.wpi.first.wpilibj.image.BinaryImage;
 import edu.wpi.first.wpilibj.image.ColorImage;
 import edu.wpi.first.wpilibj.image.EllipseMatch;
 import edu.wpi.first.wpilibj.image.EllipseDescriptor;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -31,8 +34,20 @@ public class RobotMain extends SimpleRobot {
     public static final double HEIGHT = 240;
 
     RobotDrive drive = new RobotDrive(1, 2);
+    RobotDrive arms = new RobotDrive(3, 4);
     Joystick rightStick = new Joystick(1);
     Joystick leftStick = new Joystick(2);
+    JoystickButton armsUp = new JoystickButton(leftStick, 3);
+    JoystickButton armsDown = new JoystickButton(leftStick, 2);
+    JoystickButton leftUp = new JoystickButton(rightStick, 5);
+    JoystickButton leftDown = new JoystickButton(rightStick, 3);
+    JoystickButton rightUp = new JoystickButton(rightStick, 6);
+    JoystickButton rightDown = new JoystickButton(rightStick, 4);
+    JoystickButton launch = new JoystickButton(rightStick, 2);
+    JoystickButton compressOn = new JoystickButton(leftStick, 11);
+    JoystickButton compressOff = new JoystickButton(leftStick, 10);
+    boolean compresser = false;
+    Relay relay = new Relay(1);
     AxisCamera camera;
     
     public void robotInit() {
@@ -40,7 +55,9 @@ public class RobotMain extends SimpleRobot {
     }
 
     public void autonomous() {
+        System.out.println("Autonomous mode.");
         while (isAutonomous() && isEnabled()) {
+            SmartDashboard.putString("Name", "GLaDOS");
             try {
                 ColorImage image = camera.getImage();
                 //BinaryImage thresholdImage = image.thresholdRGB(25, 255, 0, 47, 0, 47);   // keep only red objects
@@ -81,10 +98,50 @@ public class RobotMain extends SimpleRobot {
     }
 
     public void operatorControl() {
+        System.out.println("Operator control.");
         while (isOperatorControl() && isEnabled()) {
-            System.out.println("Operator control.");
+            
+            //main driving
             double mult = rightStick.getThrottle();
-            drive.tankDrive(rightStick.getY() * mult, leftStick.getY() * mult);
+            if (leftStick.getTrigger() || rightStick.getTrigger()) mult = 0.5;
+            double rightDrive = rightStick.getY();
+            double leftDrive = leftStick.getY();
+            if (leftStick.getThrottle() < 0.5) {
+                double tmp = rightDrive;
+                rightDrive = leftDrive * -1;
+                leftDrive = tmp * -1;
+            }
+            drive.tankDrive(rightDrive * mult, leftDrive * mult);
+            
+            //arm control
+            double fullArm = 1.0;
+            double leftArm = 0.0;
+            double rightArm = 0.0;
+            if (armsUp.get()) { leftArm = rightArm = fullArm; }
+            else if (armsDown.get()) { leftArm = rightArm = fullArm * -1; }
+            else {
+                if (leftUp.get()) leftArm = fullArm;
+                if (leftDown.get()) leftArm = fullArm * -1;
+                if (rightUp.get()) rightArm = fullArm;
+                if (rightDown.get()) rightArm = fullArm * -1;
+            }
+            arms.setLeftRightMotorOutputs(leftArm, rightArm);
+            
+            //compresser/launching control
+            if (compressOn.get()) { compresser = true; }
+            else if (compressOff.get()) { compresser = false; }
+            boolean forward = launch.get();
+            boolean backwards = compresser;
+            Relay.Value value = Relay.Value.kOff;
+            if (forward && backwards) {
+                value = Relay.Value.kOn;
+            } else if (forward) {
+                value = Relay.Value.kForward;
+            } else if (backwards) {
+                value = Relay.Value.kReverse;
+            }
+            relay.set(value);
+            
             Timer.delay(0.005); //do not delete
         }
     }
